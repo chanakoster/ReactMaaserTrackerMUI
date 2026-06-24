bb97dd567112cc7e78c9e408d08da34036233ba1
Chana <kosterchana@gmail.com>
2026-06-24 15:05:17 -0400

    Update controllers

---
+++ ReactMaaserTrackerMUI.Web/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReactMaaserTrackerMUI.Data;
+using ReactMaaserTrackerMUI.Web.Models;
+
+namespace ReactMaaserTrackerMUI.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public AccountController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("ConStr");
+        }
+
+        [HttpPost("addaccount")]
+        public IActionResult AddAccount(Account account)
+        {
+            var repo = new AccountRepository(_connectionString);
+            repo.AddAccount(account);
+            return Ok();
+        }
+
+        [HttpPost("updateaccount")]
+        public IActionResult UpdateAccount(Account account)
+        {
+            var repo = new AccountRepository(_connectionString);
+            repo.UpdateAccount(account);
+            return Ok();
+        }
+
+        [HttpPost("deleteaccount")]
+        public IActionResult DeleteAccount(Account account)
+        {
+            Console.WriteLine(account.Id);
+            var transactionRepo = new TransactionRepository(_connectionString);
+            transactionRepo.DeleteTransactionsForAccount(account.Id);
+            var repo = new AccountRepository(_connectionString);
+            repo.DeleteAccount(account);
+            return Ok();
+        }
+
+        [HttpGet("getallaccounts")]
+        public IActionResult GetAllAccounts()
+        {
+            var repo = new AccountRepository(_connectionString);
+            return Ok(repo.GetAllAccounts());
+        }
+
+        [HttpGet("getaccountswithtransactions")]
+        public IActionResult GetAllAccountsWithTransactions()
+        {
+            var repo = new AccountRepository(_connectionString);
+            return Ok(repo.GetAccountsWithTransactions());
+        }
+
+        [HttpPost("addincome")]
+        public IActionResult AddIncome(Transaction transaction)
+        {
+            transaction.Type = "income";
+            var repo = new TransactionRepository(_connectionString);
+            repo.AddTransaction(transaction);
+            return Ok();
+        }
+
+        [HttpGet("getaccountswithtransactionsbytype")]
+        public IActionResult GetAccountsWithTransactionsByType(string type)
+        {
+            var repo = new AccountRepository(_connectionString);
+            return Ok(repo.GetAccountsWithTransactionsByType(type));
+        }
+    }
+}
---
+++ ReactMaaserTrackerMUI.Web/Controllers/TransactionController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReactMaaserTrackerMUI.Data;
+using ReactMaaserTrackerMUI.Web.Models;
+
+namespace ReactMaaserTrackerMUI.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        //test
+        private string _connectionString;
+
+        public TransactionController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("ConStr");
+        }
+
+        [HttpGet("getallincome")]
+        public IActionResult GetAllIncomeTransactions()
+        {
+            Console.WriteLine("geting income");
+            var repo = new TransactionRepository(_connectionString);
+            foreach (var tr in repo.GetAllTransactionsByType("income"))
+            {
+                Console.WriteLine(tr.Account.Name);
+            }
+            return Ok(repo.GetAllTransactionsByType("income"));
+        }
+
+        [HttpGet("getallmaaser")]
+        public IActionResult GetAllMaaserTransactions()
+        {
+            var repo = new TransactionRepository(_connectionString);
+            return Ok(repo.GetAllTransactionsByType("maaser"));
+        }
+
+        [HttpPost("addincome")]
+        public IActionResult AddIncome(Transaction transaction)
+        {
+            Console.WriteLine("adding income");
+            transaction.Type = "income";
+            var repo = new TransactionRepository(_connectionString);
+            repo.AddTransaction(transaction);
+            return Ok();
+        }
+
+        [HttpPost("addmaaser")]
+        public IActionResult AddMaaser(Transaction transaction)
+        {
+            transaction.Type = "maaser";
+            var repo = new TransactionRepository(_connectionString);
+            repo.AddTransaction(transaction);
+            return Ok();
+        }
+
+        [HttpGet("gettotals")]
+        public IActionResult GetTotals()
+        {
+            Console.WriteLine("heyyy");
+            var repo = new TransactionRepository(_connectionString);
+            var totals = new TotalsViewModel
+            {
+                totalIncome = repo.GetTotalByType("income"),
+                totalMaaser = repo.GetTotalByType("maaser")
+            };
+            return Ok(totals);
+        }
+
+    }
+}
